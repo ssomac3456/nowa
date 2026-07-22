66ec12840f7a0a46a986c9933a7d088f921c3fb7
ssomac3456 <alwjd2dk@gmail.com>
2026-07-22 17:18:07 +0900

    7.22.8

--- js/main.js
+++ js/main.js
@@ -5,13 +5,16 @@ for (let i = 0; i < faqItems.length; i++) {
 
   question.addEventListener("click", function () {
     const isActive = faqItems[i].classList.contains("active");
+    const icon = faqItems[i].querySelector(".faq_icon img"); 
 
     for (let j = 0; j < faqItems.length; j++) {
       faqItems[j].classList.remove("active");
+      icon.src = "img/faq/+_btn.png";
     }
 
     if (!isActive) {
       faqItems[i].classList.add("active");
+      icon.src = "img/faq/+_btn_2.png";
     }
   });
 }
