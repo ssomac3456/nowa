b124f49bc58043f0eb196c1fb4e800c8e8c29a0f
ssomac3456 <cbrcbr219@naver.com>
2026-07-23 09:56:02 +0900

    7.23.1

--- js/main.js
+++ js/main.js
@@ -5,24 +5,19 @@ for (let i = 0; i < faqItems.length; i++) {
 
   question.addEventListener("click", function () {
     const isActive = faqItems[i].classList.contains("active");
-    const icon = faqItems[i].querySelector(".faq_icon img"); 
 
     for (let j = 0; j < faqItems.length; j++) {
       faqItems[j].classList.remove("active");
-      icon.src = "img/faq/+_btn.png";
+
+      const otherIcon = faqItems[j].querySelector(".faq_icon img");
+      otherIcon.src = "img/faq/+_btn.png";
     }
 
     if (!isActive) {
       faqItems[i].classList.add("active");
+
+      const icon = faqItems[i].querySelector(".faq_icon img");
       icon.src = "img/faq/+_btn_2.png";
     }
   });
 }
-
-/* for (let i = 0; i < faqItems.length; i++) {
-  const question = faqItems[i].querySelector(".faq_question");
-
-  question.addEventListener("click", function () {
-    faqItems[i].classList.toggle("active");
-  });
-} */
